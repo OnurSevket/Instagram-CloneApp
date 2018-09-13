bb5c0c27efd0bad74e0f0a701b88c254f7bc0950
OnurSevket <onursevket44@gmail.com>
2018-09-14 00:14:43 +0300

    fragments and layout created

---
+++ app/src/main/java/io/github/onursevket/instagramcloneui/Fragment/FragmentTakePhoto.java
@@ -0,0 +1,23 @@
+package io.github.onursevket.instagramcloneui.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.onursevket.instagramcloneui.R;
+
+/**
+ * Created by Onur on 13.09.2018.
+ */
+
+public class FragmentTakePhoto extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_layout_takephoto,container,false);
+        return  view;
+    }
+}
